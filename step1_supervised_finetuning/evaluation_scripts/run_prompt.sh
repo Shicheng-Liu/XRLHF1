0639f027f21803488855402c6387270b73a46766
Liu <sfl5539@psu.edu>
2025-04-28 19:04:57 -0400

    add

--- step1_supervised_finetuning/evaluation_scripts/run_prompt.sh
+++ step1_supervised_finetuning/evaluation_scripts/run_prompt.sh
@@ -8,4 +8,4 @@
 
 CUDA_VISIBLE_DEVICES=1 python prompt_eval.py \
     --model_name_or_path_baseline facebook/opt-1.3b \
-    --model_name_or_path_finetune output/opt-1.3b/tldr
+    --model_name_or_path_finetune output/opt-1.3b/Dahoas/full-hh-rlhf
