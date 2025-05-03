36f5c5fcbfa4a704df2feb0947fad9cdbe2945d1
Siyuan Xu <786912649@qq.com>
2025-05-03 11:55:02 -0400

    add

--- step3_rlhf_finetuning/reward_evaluation/reward_eval.sh
+++ step3_rlhf_finetuning/reward_evaluation/reward_eval.sh
@@ -6,7 +6,7 @@ MODEL_NAME="PKU-Alignment/beaver-7b-v3.0-reward"
 #MODEL_NAME=~/workspace/siyuan/ReMax/step2_reward_model_finetuning/output/opt-1.3b/full-hh-rlhf
 
 CUDA_VISIBLE_DEVICES=1 python reward_eval.py \
-    --data_path opt-1.3b_tldr_test_result.json \
+    --data_path opt-1.3b_test_result.json \
     --model_name_or_path_reward $MODEL_NAME \
     --data_name tldr
     
