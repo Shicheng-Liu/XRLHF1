0ef4845c30a64e6aac24b1d9ad71bd5912431a77
Siyuan Xu <786912649@qq.com>
2025-05-02 22:19:11 -0400

    add

--- step4_xrlhf_finetuning/reward_evaluation/opt/reward_1.3b_unssatisfactory.sh
+++ step4_xrlhf_finetuning/reward_evaluation/opt/reward_1.3b_unssatisfactory.sh
@@ -5,7 +5,7 @@
 MODEL_NAME="PKU-Alignment/beaver-7b-v3.0-reward"
 #MODEL_NAME=~/workspace/siyuan/ReMax/step2_reward_model_finetuning/output/opt-1.3b/full-hh-rlhf
 
-CUDA_VISIBLE_DEVICES=6 python reward_eval_unsatisfactory.py \
+CUDA_VISIBLE_DEVICES=1 python reward_eval_unsatisfactory.py \
     --data_path /gpuhome/hbz5148/workspace/siyuan/ReMax/step3_rlhf_finetuning/opt-1.3b_unsatisfactory.json \
     --new_data_path opt-1.3b_test_unsatisfactory_result.json \
     --model_name_or_path_reward $MODEL_NAME 
